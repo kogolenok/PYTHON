08e01987f0d6e435c14da095b3312bec8bcfe175
kogolenok <kogolenoko@gmail.com>
2024-07-08 22:10:40 +0300

    1

---
+++ main.py
@@ -0,0 +1,47 @@
+#print ('Hello Petruha')
+#print (5, 8, 6)
+# a=123
+# b=1.23
+# c=None
+# print (a)
+# print (b)
+# print (c)
+# a=6
+# print(type(a))
+"""
+s='df\'gh "fbs"'
+print(s)
+"""
+# a=2
+# d=1.89
+# c='ok'
+# #print (a, ' ', d, ' ', c)
+# #print (f"{a}  {d}  {c}")
+# print("{} - {} - {}".format(a,d,c))
+# print('Введите а')
+# a=input()
+# print('Введите b')
+# b=input()
+# print(a, '+', b, '=', a + b)
+# c = int (a) + int (b)
+# print(c)
+
+# print('Введите а')
+# a=input()
+# b = input('Введите b= ')
+# c = int (a) + int (b)
+# print(c)
+
+# print('Введите а')
+# a=input()
+# b = input('Введите b= ')
+# c = int (a) + float (b)
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+
+# print('Введите а=')
+# a = int (input())
+# b= int (input('Введите b='))
+# print(a, '+', b, '=', a+b)
